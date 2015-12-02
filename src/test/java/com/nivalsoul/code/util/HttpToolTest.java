98ea1a0e7f6609efc7950c4f4aa1d3d2400254fc
nivalsoul <kskscr@163.com>
2015-12-02 13:20:22 +0800

    增加HttpTool

---
+++ src/test/java/com/nivalsoul/code/util/HttpToolTest.java
@@ -0,0 +1,23 @@
+package com.nivalsoul.code.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class HttpToolTest extends TestCase {
+	String url = "http://114.215.239.66:8080/sstcd/mainServlet";
+
+	public void testGet() {
+		String result = HttpTool.get(url+"?opt=setActivityInfos");
+		System.out.println(result);
+	}
+
+	public void testPost() {
+		Map<String, String> params = new HashMap<>();
+		params.put("opt", "setActivityInfos");
+		String result = HttpTool.post(url, params );
+		System.out.println(result);
+	}
+
+}
